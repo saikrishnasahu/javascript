9918570081ec5ab6978f07f9b80d9ad66cfb1bbd
Sai Krishna Sahu <saikrishnasahu2012@gmail.com>
2023-09-30 12:24:46 +0530

    added new code

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,22 @@
+// alert('This Works !!');
+// let userName = 'Sai';
+// userName = 'Sai 1';
+// const as = 'some';
+// cannot be changed
+// as = 7;
+
+// allowed names
+// userName
+// userName3
+// $userName
+// _userName
+// user_name (not recommended)
+
+// not allowed names
+// user-nam
+// let
+
+let currentResult = 0;
+currentResult = (currentResult + 10)*3/2-1;
+
+outputResult(currentResult, 'eh');

82119ddf3d8562f602fdf99ba0bb8883b2e59829
Sai Krishna Sahu <saikrishnasahu2012@gmail.com>
2023-10-02 13:32:32 +0530

    added conditional block

---
+++ control-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,94 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+// JavaScript always looks at the first character and only considers other characters if the first character is similar.
+// In addition, capital characters are considered to be smaller than lowercase characters.
+
+// See these examples:
+
+// 'ab' > 'aa' // true
+// 'a' > 'B' // true
+// 'a' > 'b' // false
+// === checks for both value and type
+// 2 == "2" it returns true as value same
+// 2 === "2" it returns false as type different
+
+// Gets input from input field
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor file
+}
+
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function calculateResult(calculationType) {
+  if (
+    calculationType !== "ADD" &&
+    calculationType !== "SUBTRACT" &&
+    calculationType !== "MULTIPLY" &&
+    calculationType !== "DIVIDE"
+  ) {
+    return;
+  }
+
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  let mathOperator;
+  if (calculationType === "ADD") {
+    currentResult += enteredNumber;
+    mathOperator = "+";
+  } else if (calculationType === "SUBTRACT") {
+    currentResult -= enteredNumber;
+    mathOperator = "-";
+  } else if (calculationType === "MULTIPLY") {
+    currentResult *= enteredNumber;
+    mathOperator = "*";
+  } else if (calculationType === "DIVIDE") {
+    currentResult /= enteredNumber;
+    mathOperator = "/";
+  }
+
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
+
+function add() {
+  calculateResult("ADD");
+}
+
+function subtract() {
+  calculateResult("SUBTRACT");
+}
+
+function multiply() {
+  calculateResult("MULTIPLY");
+}
+
+function divide() {
+  calculateResult("DIVIDE");
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
